ec664ecffaf9902ad75c6793c18fdf0c9d952789
Sega <sealmo@gmail.com>
2021-11-09 00:44:38 +0500

    ADPy-26 Task_1

---
+++ ADPy-26/task_1/application/salary.py
@@ -0,0 +1,2 @@
+def calculate_salary(now):
+    print(f'Вызов: {__name__ } function, текущая дата: {now}')
---
+++ ADPy-26/task_1/db/people.py
@@ -0,0 +1,2 @@
+def get_employees(now):
+    print(f'Вызов: {__name__ } function, текущая дата: {now}')
